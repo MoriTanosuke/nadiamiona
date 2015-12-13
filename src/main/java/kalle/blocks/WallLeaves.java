c9ad0519110da40019e95cedca50af35c5ac3f17
MoriTanosuke <carsten@kopis.de>
2015-12-13 07:23:54 +0100

    Move code into Proxies. Also try to add Wall of leaves.

---
+++ src/main/java/kalle/blocks/WallLeaves.java
@@ -0,0 +1,83 @@
+package kalle.blocks;
+
+import java.util.List;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.BlockPos;
+import net.minecraft.util.EnumWorldBlockLayer;
+import net.minecraft.world.ColorizerFoliage;
+import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.biome.BiomeColorHelper;
+import net.minecraftforge.common.IShearable;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+import scala.actors.threadpool.Arrays;
+
+public class WallLeaves extends Wall implements IShearable {
+
+  @SideOnly(Side.CLIENT)
+  private boolean isTransparent;
+
+  public WallLeaves(CreativeTabs tab, Block modelBlock, String unlocalizedName) {
+    super(tab, modelBlock, unlocalizedName);
+    setHardness(0.2F);
+    setLightOpacity(1);
+  }
+
+  @Override
+  public boolean isOpaqueCube() {
+    return isTransparent;
+  }
+
+  @Override
+  public boolean isVisuallyOpaque() {
+    return isTransparent;
+  }
+
+  @SideOnly(Side.CLIENT)
+  public void setGraphicsLevel(boolean fancy) {
+    isTransparent = fancy;
+  }
+
+  @Override
+  @SideOnly(Side.CLIENT)
+  public EnumWorldBlockLayer getBlockLayer() {
+    return this.isTransparent ? EnumWorldBlockLayer.CUTOUT_MIPPED : EnumWorldBlockLayer.SOLID;
+  }
+
+  @Override
+  @SideOnly(Side.CLIENT)
+  public int getBlockColor() {
+    return ColorizerFoliage.getFoliageColor(0.5D, 1.0D);
+  }
+
+  @Override
+  @SideOnly(Side.CLIENT)
+  public int getRenderColor(IBlockState state) {
+    return ColorizerFoliage.getFoliageColorPine();
+  }
+
+  @Override
+  @SideOnly(Side.CLIENT)
+  public int colorMultiplier(IBlockAccess worldIn, BlockPos pos, int renderPass) {
+    return BiomeColorHelper.getFoliageColorAtPos(worldIn, pos);
+  }
+
+  @Override
+  public boolean isLeaves(IBlockAccess world, BlockPos pos) {
+    return true;
+  }
+
+  @Override
+  public boolean isShearable(ItemStack item, IBlockAccess world, BlockPos pos) {
+    return true;
+  }
+
+  @Override
+  public List<ItemStack> onSheared(ItemStack item, IBlockAccess world, BlockPos pos, int fortune) {
+    return Arrays.asList(new Object[] { new ItemStack(this) });
+  }
+}
--- src/main/java/kalle/proxies/BasisServerProxy.java
+++ src/main/java/kalle/proxies/BasisServerProxy.java
@@ -1,23 +1,24 @@
 package kalle.proxies;
 
-public class BasisClientProxy extends BasisCommonProxy {
+public class BasisServerProxy extends BasisCommonProxy {
 
   @Override
   public void preInit() {
     super.preInit();
-    LOG.info("Pre-Initializing client proxy...");
+    LOG.info("Pre-Initializing server proxy...");
   }
 
   @Override
   public void load() {
     super.load();
-    LOG.info("Loading client proxy...");
+    LOG.info("Loading server proxy...");
+
   }
 
   @Override
   public void postInit() {
     super.postInit();
-    LOG.info("Post-Initializing client proxy...");
+    LOG.info("Post-Initializing server proxy...");
   }
 
 }
