58a3adb5873f19a5accad01706463e70c8354aaa
Carsten <carsten@kopis.de>
2017-05-17 07:19:50 +0200

    Fixes #14 - Emerald Battle-Axe is no longer a tool, has damage 10 (5 hearts)

--- src/main/java/kalle/tools/ItemBattleAxeEmerald.java
+++ src/main/java/kalle/tools/ItemBattleAxeEmerald.java
@@ -1,28 +1,99 @@
 package kalle.tools;
 
+import com.google.common.collect.Multimap;
 import com.google.common.collect.Sets;
-import java.util.Set;
 import net.minecraft.block.Block;
-import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.ai.attributes.AttributeModifier;
 import net.minecraft.init.Blocks;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.ItemTool;
+import net.minecraft.util.BlockPos;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
-public class ItemBattleAxeEmerald extends ItemTool {
+import java.util.Set;
 
+public class ItemBattleAxeEmerald extends Item {
+
+    private static final ToolMaterial material = ToolMaterial.EMERALD;
+    /**
+     * Effective blocks for this weapon/tool.
+     */
     private static final Set<Block> EFFECTIVE_ON = Sets.newHashSet(new Block[]{Blocks.planks, Blocks.bookshelf, Blocks.log, Blocks.log2, Blocks.chest, Blocks.pumpkin, Blocks.lit_pumpkin, Blocks.melon_block, Blocks.ladder});
+    /**
+     * Tool efficiency on effective blocks. This axe is double efficient.
+     */
+    private static final float EFFICIENCY_ON_EFFECTIVE_BLOCKS = 2F;
+    private final float attackDamage;
 
     public ItemBattleAxeEmerald(CreativeTabs tab, String unlocalizedName) {
-        super(3.0F, ToolMaterial.EMERALD, EFFECTIVE_ON);
+        super();
         setCreativeTab(tab);
         setUnlocalizedName(unlocalizedName);
         setMaxDamage(1561 * 4);
-        // TODO need to set efficiencyOnProperMaterial?
-        efficiencyOnProperMaterial = 4.0f;
+        setMaxStackSize(1);
+        attackDamage = 10F; // 10 damage == 5 hearts
     }
 
+    public float getDamageVsEntity() {
+        return attackDamage;
+    }
+
+    // methods taken from ItemSword
+
     public float getStrVsBlock(ItemStack stack, Block state) {
-        return state.getMaterial() != Material.wood && state.getMaterial() != Material.plants && state.getMaterial() != Material.vine ? super.getStrVsBlock(stack, state) : this.efficiencyOnProperMaterial;
+        if (state == Blocks.web) {
+            return 15.0F;
+        } else {
+            return EFFECTIVE_ON.contains(state) ? EFFICIENCY_ON_EFFECTIVE_BLOCKS : 1.0F;
+        }
+    }
+
+    public boolean hitEntity(ItemStack stack, EntityLivingBase target, EntityLivingBase attacker) {
+        stack.damageItem(1, attacker);
+        return true;
+    }
+
+    public boolean onBlockDestroyed(ItemStack stack, World worldIn, Block blockIn, BlockPos pos, EntityLivingBase playerIn) {
+        if ((double) blockIn.getBlockHardness(worldIn, pos) != 0.0D) {
+            stack.damageItem(2, playerIn);
+        }
+
+        return true;
+    }
+
+    public boolean canHarvestBlock(IBlockState blockIn) {
+        return blockIn.getBlock() == Blocks.web;
+    }
+
+    @SideOnly(Side.CLIENT)
+    public boolean isFull3D() {
+        return true;
+    }
+
+    public int getItemEnchantability() {
+        return this.material.getEnchantability();
+    }
+
+    public String getToolMaterialName() {
+        //TODO return emerald name
+        return material.toString();
+    }
+
+    public boolean getIsRepairable(ItemStack toRepair, ItemStack repair) {
+        ItemStack mat = this.material.getRepairItemStack();
+        if (mat != null && net.minecraftforge.oredict.OreDictionary.itemMatches(mat, repair, false)) return true;
+        return super.getIsRepairable(toRepair, repair);
+    }
+
+    public Multimap<String, AttributeModifier> getItemAttributeModifiers() {
+        Multimap<String, AttributeModifier> multimap = super.getItemAttributeModifiers();
+        multimap.put(SharedMonsterAttributes.attackDamage.getAttributeUnlocalizedName(), new AttributeModifier(itemModifierUUID, "Weapon modifier", (double) this.attackDamage, 0));
+        return multimap;
     }
 }
