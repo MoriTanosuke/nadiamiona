f44bcf845f1c5972ec35f5bcb8214c181dd5cb5f
Carsten <carsten@kopis.de>
2017-05-25 11:44:50 +0200

    Set update.json to latest version

--- src/main/java/kalle/Basis.java
+++ src/main/java/kalle/Basis.java
@@ -1,6 +1,5 @@
 package kalle;
 
-import org.apache.logging.log4j.Logger;
 import kalle.proxies.BasisCommonProxy;
 import net.minecraftforge.fml.common.FMLLog;
 import net.minecraftforge.fml.common.Mod;
@@ -10,9 +9,10 @@ import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
 
 //TODO replace with build.gradle properties
-@Mod(modid = "kalle", name = "Nadiamiona", version = "1.11.2-1", updateJSON = "https://raw.githubusercontent.com/MoriTanosuke/nadiamiona/master/update.json")
+@Mod(modid = "kalle", name = "Nadiamiona", version = "1.11.2-2", updateJSON = "https://raw.githubusercontent.com/MoriTanosuke/nadiamiona/master/update.json")
 public class Basis {
   private static final Logger LOG = FMLLog.getLogger();
   public static final String MOD_PREFIX = "kalle";
