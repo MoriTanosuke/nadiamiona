f1d4c974dc1be00fd7714177e00aba27acd84c23
Carsten <carsten@kopis.de>
2018-01-09 20:21:51 +0100

    Remove more unused code

--- src/main/java/kalle/recipes/Recipes.java
+++ src/main/java/kalle/recipes/Recipes.java
@@ -1,38 +1,11 @@
 package kalle.recipes;
 
-import kalle.Basis;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.registry.GameRegistry;
-import net.minecraftforge.oredict.RecipeSorter;
 
 public class Recipes {
     public static void register() {
-        net.minecraftforge.oredict.RecipeSorter.register(Basis.MOD_PREFIX + ".potionRecipe", PotionRecipe.class, RecipeSorter.Category.SHAPED, "after:minecraft:shaped");
-
-        // --- tool recipes ---------------------------------------------------------
-        /* TODO fix registering of PotionRecipe for ItemExcavator
-        ItemStack swiftness = PotionUtils.addPotionToItemStack(new ItemStack(Items.POTIONITEM), PotionTypes.STRONG_SWIFTNESS);
-        ItemStack strength = PotionUtils.addPotionToItemStack(new ItemStack(Items.POTIONITEM), PotionTypes.STRONG_STRENGTH);
-        GameRegistry.addRecipe(new PotionRecipe(3, 3,
-                new ItemStack[]{
-                        // first row
-                        new ItemStack(net.minecraft.init.Blocks.EMERALD_BLOCK),
-                        new ItemStack(net.minecraft.init.Blocks.EMERALD_BLOCK),
-                        new ItemStack(net.minecraft.init.Blocks.EMERALD_BLOCK),
-                        // second row
-                        swiftness,
-                        new ItemStack(net.minecraft.init.Blocks.DIAMOND_BLOCK),
-                        strength,
-                        // third row
-                        ItemStack.EMPTY,
-                        new ItemStack(net.minecraft.init.Blocks.DIAMOND_BLOCK),
-                        ItemStack.EMPTY,
-                },
-                new ItemStack(kalle.items.Items.EXCAVATOR)
-        ));
-        */
-
         // --- food recipes ---------------------------------------------------------
         GameRegistry.addSmelting(net.minecraft.init.Items.EGG, new ItemStack(kalle.items.Items.COOKED_EGG), 0.25f);
         GameRegistry.addSmelting(kalle.items.Items.BREADSLICE, new ItemStack(kalle.items.Items.TOAST), 0.25f);
